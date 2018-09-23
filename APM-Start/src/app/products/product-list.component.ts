33d7fc51ec0beb63ac3e03fe0c64542ee876e863
Lena Koester <koesterlen@gmail.com>
2018-09-23 13:17:25 +0200

    Added nested component with star rating

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -50,6 +50,10 @@ export class ProductListComponent implements OnInit {
     this.listFilter = 'cart';
   }
 
+  onRatingClicked(message:string): void{
+    this.pageTitle = this.pageTitle + message;
+  }
+
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) =>
