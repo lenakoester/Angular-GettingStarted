7e1fe1bce845197dcd56af54b9c982f9231edf59
Lena Koester <lkoester@Lenas-MacBook-Pro.local>
2018-09-24 14:22:10 +0200

    Products now called with http routing and handled as an Observable

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService} from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -11,6 +12,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage:string;
 
   _listFilter: string;
   get listFilter(): string {
@@ -22,31 +24,9 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2016",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    }
-  ];
+  products: IProduct[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
+  constructor(private productService: ProductService) {
     this.listFilter = 'cart';
   }
 
@@ -65,6 +45,13 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products,
+        this.filteredProducts = this.products;
+      }
+      error => this.errorMessage = <any>error
+    )
+    this.filteredProducts = this.products;
   }
 }
---
+++ APM-Start/src/app/products/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap} from 'rxjs/operators';
+import { IProduct } from './product';
+
+@Injectable ({
+  providedIn: 'root'
+})
+
+export class ProductService {
+  private productUrl = 'api/products/products.json';
+
+  constructor (private http: HttpClient) {}
+
+  getProducts(): Observable<IProduct[]>{
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: '+ JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError (err: HttpErrorResponse) {
+    // code missing here, see tutorial 10
+    let errorMessage = '';
+    console.error(errorMessage);
+    return throwError(errorMessage)
+  }
+
+}
