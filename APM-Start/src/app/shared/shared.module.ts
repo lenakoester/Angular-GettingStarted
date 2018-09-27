104da213f893f87bc937b7abb3eed420e9f54b8a
Lena <Lena.Koester@akquinet.de>
2018-09-27 14:15:59 +0200

    Last change to shared module

--- APM-Start/src/app/shared/shared.module.ts
+++ APM-Start/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StarComponent} from './star.component';
 import { FormsModule} from '@angular/forms';
+
 @NgModule({
   imports: [
     CommonModule
